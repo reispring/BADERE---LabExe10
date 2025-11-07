e9c8e3d2eab6ceb1e5909852f96ffb77705ca222
reispring <andreimbadere@gmail.com>
2025-11-07 18:52:30 +0800

    Add project files.

---
+++ BADERE - LabExe10/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNumbers = New System.Windows.Forms.TextBox()
+        Me.btnSaveSort = New System.Windows.Forms.Button()
+        Me.Sorted = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(286, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter numbers: "
+        '
+        'txtNumbers
+        '
+        Me.txtNumbers.Location = New System.Drawing.Point(279, 71)
+        Me.txtNumbers.Name = "txtNumbers"
+        Me.txtNumbers.Size = New System.Drawing.Size(210, 20)
+        Me.txtNumbers.TabIndex = 1
+        '
+        'btnSaveSort
+        '
+        Me.btnSaveSort.Location = New System.Drawing.Point(332, 107)
+        Me.btnSaveSort.Name = "btnSaveSort"
+        Me.btnSaveSort.Size = New System.Drawing.Size(100, 23)
+        Me.btnSaveSort.TabIndex = 2
+        Me.btnSaveSort.Text = "Save and Sort"
+        Me.btnSaveSort.UseVisualStyleBackColor = True
+        '
+        'Sorted
+        '
+        Me.Sorted.FormattingEnabled = True
+        Me.Sorted.Location = New System.Drawing.Point(279, 171)
+        Me.Sorted.Name = "Sorted"
+        Me.Sorted.Size = New System.Drawing.Size(210, 147)
+        Me.Sorted.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Thistle
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Sorted)
+        Me.Controls.Add(Me.btnSaveSort)
+        Me.Controls.Add(Me.txtNumbers)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNumbers As TextBox
+    Friend WithEvents btnSaveSort As Button
+    Friend WithEvents Sorted As ListBox
+End Class
---
+++ BADERE - LabExe10/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub btnSaveSort_Click(sender As Object, e As EventArgs) Handles btnSaveSort.Click
+        Try
+            Dim input As String = txtNumbers.Text
+
+            ' Write to file
+            Using writer As New StreamWriter("numbers.txt")
+                writer.WriteLine(input)
+            End Using
+
+            ' Read from file
+            Dim fileContent As String
+            Using reader As New StreamReader("numbers.txt")
+                fileContent = reader.ReadToEnd()
+            End Using
+
+            ' Convert to list of integers
+            Dim numbers As List(Of Integer) = fileContent.Split(" "c).Select(Function(n) Integer.Parse(n.Trim())).ToList()
+
+            ' Sort numbers using LINQ OrderBy
+            Dim sortedNumbers = numbers.OrderBy(Function(n) n).ToList()
+
+            ' Display in ListBox
+            Sorted.Items.Clear()
+            For Each num In sortedNumbers
+                Sorted.Items.Add(num)
+            Next
+
+
+            Console.WriteLine("Sorted Numbers:")
+            For Each num In sortedNumbers
+                Console.WriteLine(num)
+            Next
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        End Try
+    End Sub
+
+End Class
+
